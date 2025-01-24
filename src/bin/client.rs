3aa529e3c810e20828de48da9e7a54572ffa3d20
Speedy_Lex <alex.ciocildau@gmail.com>
2025-01-24 14:41:47 +0100

    comments

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -10,9 +10,13 @@ fn main() {
     };
     // serialize
     let bytes = to_bytes::<Error>(&file).expect("serialize failed");
-    let mut stream = TcpStream::connect(SocketAddrV4::new(Ipv4Addr::LOCALHOST, PORT)).expect("Couldn't connect"); // connect to server
+
+    // connect
+    let mut stream = TcpStream::connect(SocketAddrV4::new(Ipv4Addr::LOCALHOST, PORT)).expect("Couldn't connect");
+
+    // send length
     stream.write_all(&(bytes.len() as u32).to_le_bytes()).unwrap();
+    // send data
     stream.write_all(&bytes).expect("can't write buf");
     stream.flush().unwrap();
-    println!("done");
 }
