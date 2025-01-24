b9e18a5272da0984c745322588a49fe9e113a71e
Speedy_Lex <alex.ciocildau@gmail.com>
2025-01-24 13:46:50 +0100

    hello world

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,10 @@
+use std::{io::Write, net::{Ipv4Addr, SocketAddrV4, TcpStream}};
+
+use nas_rs::PORT;
+
 fn main() {
-    println!("Client");
+    let mut stream = TcpStream::connect(SocketAddrV4::new(Ipv4Addr::LOCALHOST, PORT)).expect("Couldn't connect");
+    stream.write_all("Hello World!".as_bytes()).unwrap();
+    stream.flush().unwrap();
+    println!("done");
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,13 @@
+use std::{io::{BufRead, BufReader}, net::{Ipv4Addr, SocketAddrV4, TcpListener}};
+
+use nas_rs::PORT;
+
 fn main() {
-    println!("Server");
+    let listener = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::LOCALHOST, PORT)).expect("Couldn't bind port");
+    for msg in listener.incoming() {
+        let msg = msg.unwrap();
+        let mut buf = String::new();
+        BufReader::new(msg).read_line(&mut buf).unwrap();
+        println!("{buf}");
+    }
 }
