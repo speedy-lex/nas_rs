782bbaefb771f68de5005e0f580a20956f685308
Speedy_Lex <alex.ciocildau@gmail.com>
2025-02-21 13:35:28 +0100

    i/o as files

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,4 +1,4 @@
-use std::{io::{Read, Write}, net::{Ipv4Addr, SocketAddrV4, TcpStream}};
+use std::{fs::File, io::{Read, Write}, net::{Ipv4Addr, SocketAddrV4, TcpStream}};
 
 use clap::{arg, value_parser};
 use nas_rs::{ArchivedDirEnum, ArchivedFileRead, DirEnum, FileRead, Request, StructStream, PORT};
@@ -15,6 +15,7 @@ fn main() {
         ).arg(
             arg!(--write)
             .required(false)
+            .requires("in")
         ).arg(
             arg!(--mkdir)
             .required(false)
@@ -32,6 +33,11 @@ fn main() {
             arg!(--port <port>)
             .required(false)
             .value_parser(value_parser!(u16))
+        ).arg(
+            arg!(--in <in_file>)
+        ).arg(
+            arg!(--out <out_file>)
+            .required_unless_present_any(["write", "mkdir", "delete", "enumerate"])
         ).get_matches();
     
     let path = args.get_one::<String>("file_path").unwrap().clone();
@@ -41,7 +47,8 @@ fn main() {
         .filter(|x| **x)
         .map(|_: &bool| {
             let mut vec = vec![];
-            std::io::stdin().read_to_end(&mut vec).expect("can't read");
+            let mut file = args.get_one("in").map(|x: &String| Box::new(File::open(x).unwrap()) as Box<dyn Read>).unwrap_or_else(|| Box::new(std::io::stdin()));
+            file.read_to_end(&mut vec).expect("can't read");
             vec
         }).unwrap_or_default();
     // package data
@@ -82,13 +89,16 @@ fn main() {
     stream.write_struct::<Error>(&request).expect("couldn't send request");
     if let Request::Write { .. } = request {
         stream.write_buffer::<Error>(&file_data).expect("couldn't send file");
+        println!("done writing");
     }
 
     stream.inner.flush().unwrap();
     if let Request::Read { .. } = request {
         let file_info = stream.receive_struct::<FileRead, ArchivedFileRead, Error>().expect("couldn't recieve file");
         let file = stream.receive_buffer::<Error>(file_info.len).expect("couldn't receive file");
-        std::io::stdout().write_all(&file).unwrap();
+        let mut out_file = args.get_one("out").map(|x: &String| Box::new(File::create(x).unwrap()) as Box<dyn Write>).unwrap_or_else(|| Box::new(std::io::stdout()));
+        out_file.write_all(&file).unwrap();
+        out_file.flush().unwrap();
     } else if let Request::EnumDir { .. } = request {
         let files = stream.receive_struct::<DirEnum, ArchivedDirEnum, Error>().expect("couldn't receive dir enum");
         println!("{files:?}");
